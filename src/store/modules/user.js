b9348321e1deb94e69ecd7aad4c6b9094b79dfd2
sumeiling <1305992447@qq.com>
2023-09-06 16:42:18 +0800

    fix

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,79 +1,77 @@
 // import mutations from './mutations'
 import HOME from "@/pages/home/api";
-import ACCOUNT_API from '@/pages/account/api';
+import ACCOUNT_API from "@/pages/account/api";
 
 const user = {
-    namespaced: true,
-    state() {
-        return {
-            token: "",
-            permission: [],
-            filterRouter: [],
-            info: {},
-            agentInfo: {},
+  namespaced: true,
+  state() {
+    return {
+      token: "",
+      permission: [],
+      filterRouter: [],
+      info: {},
+      agentInfo: {},
+    };
+  },
+  mutations: {
+    // 设置token
+    SET_USER_TOKEN(state, val) {
+      state.token = val;
+    },
+    // 删除token
+    REMOVE_TOKEN(statem) {
+      state.token = ""; // 删除vuex的token
+    },
+    SET_FILTERROUTER_LIST(state, list) {
+      state.filterRouterList = list;
+    },
+    SET_PERMISSION_LIST(state, val) {
+      state.permission = val;
+    },
+    SET_USER_INFO(state, val) {
+      state.info = val;
+    },
+    SET_AGENT_USER_INFO(state, val) {
+      state.agentInfo = val;
+    },
+  },
+  actions: {
+    getPermissionList({ commit }) {
+      let list = [1];
+      commit("SET_PERMISSION_LIST", list);
+      return list;
+      // return new Promise((resolve, reject) => {
+
+      // });
+    },
+    getUserInfo({ commit }) {
+      HOME.getUserInfo().then((res) => {
+        if (res.code == "0") {
+          // res.data.isAdmin = 1
+          commit("SET_USER_INFO", res.data);
+          window.localStorage.setItem("isAdmin", res.data.isAdmin || 0);
+        } else {
+          commit("SET_USER_INFO", {});
         }
+      });
     },
-    mutations: {
-        // 设置token
-        SET_USER_TOKEN(state, val) {
-            state.token = val
-        },
-        // 删除token
-        REMOVE_TOKEN(statem) {
-            state.token = "" // 删除vuex的token
-        },
-        SET_FILTERROUTER_LIST(state, list) {
-            state.filterRouterList = list;
-        },
-        SET_PERMISSION_LIST(state, val) {
-            state.permission = val
-        },
-        SET_USER_INFO(state, val) {
-            state.info = val
-        },
-        SET_AGENT_USER_INFO(state, val) {
-            state.agentInfo = val
-        },
+    getAgentUserInfo({ commit }) {
+      ACCOUNT_API.getAgentUserInfo().then((res) => {
+        if (res.code == "0") {
+          commit("SET_AGENT_USER_INFO", res.data);
+          window.localStorage.setItem("roleId", res.data.roleId || 10);
+        } else {
+          commit("SET_AGENT_USER_INFO", {});
+        }
+      });
     },
-    actions: {
-        getPermissionList({ commit }) {
-            let list = [1];
-            commit('SET_PERMISSION_LIST', list);
-            return list;
-            // return new Promise((resolve, reject) => {
+  },
+  getters: {
+    permissionList: (state) => state.permission,
+    info: (state) => state.info,
+    filterRouter: (state) => state.filterRouter,
+    agentInfo: (state) => state.agentInfo,
+  },
+};
 
-            // });
-        },
-        getUserInfo({ commit }) {
-            HOME.getUserInfo().then(res=>{
-                if (res.code == '0') {
-                    // res.data.isAdmin = 1
-                    commit('SET_USER_INFO', res.data);
-                    window.localStorage.setItem('isAdmin', res.data.isAdmin || 0)
-                }else {
-                    commit('SET_USER_INFO', {});
-                }
-            })
-        },
-        getAgentUserInfo({commit}) {
-            ACCOUNT_API.getAgentUserInfo().then(res => {
-                if (res.code == '0') {
-                    commit('SET_AGENT_USER_INFO', res.data);
-                    window.localStorage.setItem('roleId', res.data.roleId || 10)
-                } else {
-                    commit('SET_AGENT_USER_INFO', {});
-                }
-            })
-        },
-    },
-    getters: {
-        permissionList: state => state.permission,
-        info: state => state.info,
-        filterRouter: state => state.filterRouter,
-        agentInfo: state => state.agentInfo,
-    }
-}
-
-
-
-export default user
+export default user;
