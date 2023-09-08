14bfee236d6dcd971f55fb116a005eb747070800
sumeiling <1305992447@qq.com>
2023-09-08 10:04:00 +0800

    'fix'

--- src/pages/distributionAdm/financialSystem/index.vue
+++ src/pages/distributionAdm/financialSystem/index.vue
@@ -248,23 +248,34 @@
           :data="tableDialogData"
           style="width: 100%"
           @selection-change="handleSelectionChange"
+          height="400"
         >
-          <el-table-column
+          <template
             v-for="(item, index) in tableDialogColumnConfig"
             :key="index"
-            :prop="item.prop"
-            :label="item.label"
-            :width="item.width"
-            align="center"
           >
-            <template #default="{ row, column, $index }">
-              <div v-if="item.insertSlot && item.prop === 'operate'">
-                <el-button type="danger" link @click="deleteOrder(row)"
-                  >删除
-                </el-button>
-              </div>
-            </template>
-          </el-table-column>
+            <el-table-column
+              v-if="currentStatus != 0 && item.prop != 'operate'"
+              :prop="item.prop"
+              :label="item.label"
+              :width="item.width"
+              align="center"
+            >
+              <template #default="{ row, column, $index }">
+                <div
+                  v-if="
+                    item.insertSlot &&
+                    item.prop == 'operate' &&
+                    currentStatus == 0
+                  "
+                >
+                  <el-button type="danger" link @click="deleteOrder(row)"
+                    >删除
+                  </el-button>
+                </div>
+              </template>
+            </el-table-column>
+          </template>
         </el-table>
 
         <div class="u-pagination-container">
@@ -557,13 +568,15 @@ const tableDialogColumnConfig = ref([
   { label: "客户名称", prop: "customName" },
   { label: "订单金额", prop: "orderAmount" },
   { label: "结算佣金", prop: "orderCommission" },
-  { label: "操作", prop: "operate", insertSlot: "operate" },
+  { label: "操作", prop: "operate", insertSlot: "operate", waitCheck: true },
 ]);
 
+const currentStatus = ref();
 const openEditDialog = (row) => {
   eidtDialogVisible.value = true;
   currentDialogId.value = row.withdrawId;
   searchDialogTableParams.keyWords = undefined;
+  currentStatus.value = row.status;
   handleGetDialogTableList(row.withdrawId);
 };
 
